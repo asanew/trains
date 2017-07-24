05c4a898ba4655ddb2024515b3d327b2d9932556
Asanov Sergey <asanew@bk.ru>
2017-07-24 13:58:08 +0700

    Many-to-many completed

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -2,4 +2,6 @@ class RailwayStation < ApplicationRecord
   has_many :trains, foreign_key: :current_station_id
   has_many :start_tickets, class_name: 'Ticket', foreign_key: :start_station_id
   has_many :end_tickets, class_name: 'Ticket', foreign_key: :end_station_id
+  has_many :railway_stations_routes
+  has_many :routes, through: :railway_stations_routes
 end
