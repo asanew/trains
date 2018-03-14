5cbd4b469080c883f7961900de53b075f72cc7e4
Sergey Asanov <asanew@bk.ru>
2018-03-14 13:45:22 +0700

    Wagons completed

--- app/models/train.rb
+++ app/models/train.rb
@@ -4,11 +4,16 @@ class Train < ApplicationRecord
   has_many :tickets
   has_many :wagons
 
-  #def get_wagons_data
-  #  result = {}
-  #  WagonType.each do |wt|
-  #    wagons_by_type = wagons.where(wagon_type: wt)
-  #    result[wt] = [wagons_by_type.count, ]
-  #  end
-  #end
+  def get_wagons_data
+    result = {}
+    WagonType.all.each do |wt|
+      wagons_by_type = wagons.where(wagon_type: wt)
+      result[wt] = { quantity: wagons_by_type.count, top_seats: 0, bottom_seats: 0 }
+      wagons_by_type.each do |wagon|
+        result[wt][:top_seats] += wagon.top_seats
+        result[wt][:bottom_seats] += wagon.bottom_seats
+      end
+    end
+    return result
+  end
 end
