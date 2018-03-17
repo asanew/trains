aefb032deafcce957b3f2355eeafc0afc84e7b61
Sergey Asanov <asanew@bk.ru>
2018-03-17 19:33:36 +0700

    Station sort completed

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -5,5 +5,4 @@ class RailwayStationsRoute < ApplicationRecord
   validates :railway_station_id, uniqueness: { scope: :route_id }
   validates :railway_station_number, uniqueness: { scope: :route_id }
 
-  default_scope { order(:railway_station_number) }
 end
