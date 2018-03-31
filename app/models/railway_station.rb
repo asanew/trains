2571b076f7bf0f507ccb1f0e57205bbaf1986e18
Sergey Asanov <asanew@bk.ru>
2018-03-31 20:01:08 +0700

    Sort completed

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -5,5 +5,18 @@ class RailwayStation < ApplicationRecord
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
 
+  def update_position(route, position)
+    station_route = station_route(route)
+    station_route.update(railway_station_number: position) if @station_route
+  end
+
+  def position_in(route)
+    station_route(route).try(:railway_station_number)
+  end
+
+  def station_route(route)
+    @station_route ||= railway_stations_routes.where(route: route).first
+  end
+
   scope :number_sort, -> { order('railway_stations_routes.railway_station_number').all }
 end
